07ba226c7782804e982f5d828d082c4f8aca5699
Mukul Sharma <mukul6307@gmail.com>
2025-11-26 15:32:30 +0530

    new code

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,11 @@ Route::get('/sitemap.xml', function () {
     return response()->file(public_path('sitemap.xml'));
 });
 
+Route::get('/sitemap-generate', function () {
+    Artisan::call('sitemap:generate');
+    return 'cron executed';
+});
+
 Route::options('{any}', function () {
     return response()->noContent();
 })->where('any', '.*');
